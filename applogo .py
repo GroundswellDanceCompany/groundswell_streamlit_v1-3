c34338e1f32175827c11fc2d238db047bbb379ad
GroundswellDanceCompany <amandakaniewska46@gmail.com>
2025-04-12 21:09:50 +0100

    Delete applogo .py

--- applogo .py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="Groundswell Dance Portal", layout="centered")
-st.markdown("<div style='text-align: center;'><img src='groundswell_logo.jpg' width='400'></div>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: white; background-color: black; padding: 10px;'>Groundswell Dance Company</h1>", unsafe_allow_html=True)
-st.markdown("### Everything is ready. The full app functionality is now being wrapped into this version.")
