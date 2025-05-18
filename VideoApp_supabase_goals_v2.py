6a6f5d0376fc7437ce04fd535161c8e569001a28
GroundswellDanceCompany <amandakaniewska46@gmail.com>
2025-05-18 23:25:48 +0100

    Update VideoApp_supabase_goals_v2.py

--- VideoApp_supabase_goals_v2.py
+++ VideoApp_supabase_goals_v2.py
@@ -235,7 +235,7 @@ elif st.session_state.logged_in:
             st.subheader("Upload Class Resource Videos")
 
             teacher_videos_file = "teacher_videos# JSON filename removed (Supabase used)"
-            teacher_videos = # load_json removed (Supabase used)(teacher_videos_file, [])
+            teacher_videos = supabase.table("teacher_videos").select("*").execute().data
 
             # Upload section
             video_label = st.text_input("Video Label")
