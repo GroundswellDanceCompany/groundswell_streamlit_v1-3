2b5093c1279813ee8c6445332c7accb72affc730
GroundswellDanceCompany <amandakaniewska46@gmail.com>
2025-05-18 10:17:35 +0100

    Update VideoApp.py

--- VideoApp.py
+++ VideoApp.py
@@ -1,12 +1,10 @@
 
 import streamlit as st
+st.set_page_config(page_title="Groundswell Goal Tracker", layout="centered")
 import uuid
 import datetime
 import json
 import os
-import streamlit as st
-
-st.set_page_config(page_title="Groundswell Goal Tracker", layout="centered")
 
 from supabase import create_client, Client
 
