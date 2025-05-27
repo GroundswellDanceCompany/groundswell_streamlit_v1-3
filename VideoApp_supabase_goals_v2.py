58f33fe61342e846a85331db17f82eb68b6bee56
GroundswellDanceCompany <amandakaniewska46@gmail.com>
2025-05-27 10:21:48 +0100

    Update VideoApp_supabase_goals_v2.py

--- VideoApp_supabase_goals_v2.py
+++ VideoApp_supabase_goals_v2.py
@@ -221,6 +221,7 @@ elif st.session_state.logged_in:
     "id": st.session_state.get("user_id"),
     "role": st.session_state.get("user_role", "student"),  # default to student
     "groups": st.session_state.user_groups
+    }
 }
     is_teacher = user_info["role"] == "admin"
     st.sidebar.title(f"Hello, {user}")
